e8612121c7fa291e8334753134662e3921904f1e
CynaraCosta <cvocac@cin.ufpe.br>
2024-03-17 21:43:40 -0300

    added the widgets

---
+++ desing_patterns_flutter/lib/patterns/builder/widgets/card_footer_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardFooterWidget extends StatelessWidget {
+  const CardFooterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ desing_patterns_flutter/lib/patterns/builder/widgets/card_header_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardHeaderWidget extends StatelessWidget {
+  const CardHeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
